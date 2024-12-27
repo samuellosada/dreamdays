2456dc0aaea603e542686819ee5a916146b8cb25
Samuel Losada Mora <samthesam26@gmail.com>
2024-12-27 11:24:38 +1100

    Created colorpicker component and applied to dreamcard. pending integration into addnewdream. Can save color changes when changed during editing.

---
+++ src/lib/components/ColorPicker.svelte
@@ -0,0 +1,37 @@
+<script>
+    let { color =  $bindable() } = $props();
+</script>
+
+<!-- This component needs colour to be passed back to the deamcard to be saved. but the initial colour must also be passed -->
+ <!-- it needs to be passed because it will be used to both make new dreams and edit existing that already have a chosen dream -->
+<!-- svelte-ignore a11y_consider_explicit_label -->
+<div class="grid grid-cols-4 gap-4 mt-8">
+    <button 
+        class="w-16 h-16 rounded-full bg-blue-500 hover:opacity-80 transition-opacity"
+        onclick={() => color = '#3B82F6'}
+    ></button>
+    <button 
+        class="w-16 h-16 rounded-full bg-emerald-500 hover:opacity-80 transition-opacity"
+        onclick={() => color = '#10B981'}
+    ></button>
+    <button 
+        class="w-16 h-16 rounded-full bg-red-500 hover:opacity-80 transition-opacity"
+        onclick={() => color = '#EF4444'}
+    ></button>
+    <button 
+        class="w-16 h-16 rounded-full bg-yellow-500 hover:opacity-80 transition-opacity"
+        onclick={() => color = '#F59E0B'}
+    ></button>
+    <button 
+        class="w-16 h-16 rounded-full bg-indigo-500 hover:opacity-80 transition-opacity"
+        onclick={() => color = '#6366F1'}
+    ></button>
+    <button 
+        class="w-16 h-16 rounded-full bg-orange-500 hover:opacity-80 transition-opacity"
+        onclick={() => color = '#F97316'}
+    ></button>
+    <button 
+        class="w-16 h-16 rounded-full bg-pink-500 hover:opacity-80 transition-opacity"
+        onclick={() => color = '#EC4899'}
+    ></button>
+</div>
--- src/lib/components/DreamCard.svelte
+++ src/lib/components/DreamCard.svelte
@@ -1,5 +1,6 @@
 <script>
     import { dreams } from '$lib/stores';
+	import ColorPicker from './ColorPicker.svelte';
 
     // WHAT TIME IS THE STREAK BEING RESET AT? 
     // Finish the Editing mode. 
@@ -8,9 +9,9 @@
 
     //BUGS
     // When editing, if text is longer, it overflows the card.
+    //sometimes the placeholder for the textarea when editing defaults to and empty string.
 
-    let props = $props();
-    let dream = props.dream
+    let { dream } = $props();
 
     const today = new Date().toISOString().split('T')[0];
     let dreamLastUpdate = $state(dream.lastUpdated)
@@ -19,17 +20,21 @@
     let isMarkedToday = $derived(dreamLastUpdate === today);
 
     let isEditing = $state(false)
+    let editedDescription = $state(dream.description);
+    let editedColor = $state(dream.color);
+    let editingStep = $state(1);
 
     function handleUpdate() {
         dreams.update(currentDreams => 
             currentDreams.map(currentDream => {
                 if (currentDream.id === dream.id) {
                     return {
-                        ...currentDream,
-                        description: currentDream.description
+                    ...currentDream,
+                    color: editedColor,
+                    description: editedDescription
                     };  
                 }
-                return currentDream;
+            return currentDream;
             })
         );
         cancelEditing();
@@ -65,8 +70,26 @@
     }
 
     function cancelEditing() {
+        editedDescription = '';
         isEditing = false;
+        editingStep = 1;
     }
+
+    function nextStep() {
+    if (editingStep < 3) {
+        editingStep++;
+        console.log(editingStep);
+    } else {
+    
+        handleUpdate();
+    }
+
+    function previousStep() {
+    if (editingStepStep > 0) {
+        editingStep--;
+    }
+}
+}
 
 //ENSURE YOU MAKE AN ALTERNATIVE FOR DOUBLE CLICK ON MOBILE AT LEAST?! LONG HOLD?
 </script>
@@ -105,17 +128,19 @@
         p-6 h-full xl:h-[80%] min-w-[85%] md:min-w-[60%] xl:min-w-[45%] mr-6 rounded-3xl shadow-lg transition-colors 
         snap-center bg-slate-50 text-slate-950"
     >  
-    <!-- FOR SOME REASON I CAN STILL SEE A BORDER WHEN THE TEXT AREA IS FOCUSED. -->
-        <textarea
-            bind:value={dream.description}
-            class="w-full min-h-[90%] xl:min-h-[88%] p-0 text-4xl font-semibold leading-10 border-0 focus:border-transparent focus:border-0 focus:border-none focus:outline-none resize-none bg-slate-50"
-            placeholder={dream.description}
-            required="true"
-        >
-        {dream.description}
-        </textarea>
-        <!-- svelte-ignore a11y_consider_explicit_label -->
-        <div class="flex justify-between items-center">
+        {#if editingStep === 1} 
+            <!-- FOR SOME REASON I CAN STILL SEE A BORDER WHEN THE TEXT AREA IS FOCUSED. -->
+            <textarea
+                bind:value={editedDescription}
+                class="w-full min-h-[90%] xl:min-h-[88%] p-0 text-4xl font-semibold leading-10 border-0 focus:border-transparent focus:border-0 focus:border-none focus:outline-none resize-none bg-slate-50"
+                placeholder={editedDescription}
+                required="true"
+            >
+            {editedDescription}
+            </textarea>
+
+            <!-- svelte-ignore a11y_consider_explicit_label -->
+            <div class="flex justify-between items-center">
             <button type="button" class=""
             onclick={() => {handleDelete()}}>Delete</button>
             </div>
@@ -128,9 +153,44 @@
             <button
             type="button"
             class="flex gap-2 font-medium hover:opacity-70 transition-opacity"
-            onclick={handleUpdate}>Confirm</button>
-
-
+            onclick={nextStep}>Next</button>
+            
+        {:else if editingStep === 2}        
+            <div class="space-y-6">
+                <h1 class="text-6xl font-bold">MY DREAM<br>IS GIVING...</h1>
+                <ColorPicker bind:color={editedColor}/>
+            </div>
+            <div class="flex justify-between items-center">
+                <button type="button" class=""
+                onclick={() => {handleDelete()}}>Delete</button>
+                </div>
+    
+                <button
+                type="button"
+                class="flex gap-2 font-medium hover:opacity-70 transition-opacity"
+                onclick={cancelEditing}>Cancel</button>
+    
+                <button
+                type="button"
+                class="flex gap-2 font-medium hover:opacity-70 transition-opacity"
+                onclick={nextStep}>Next</button>
+        {:else if editingStep === 3}
+            <h1>idk what to put here yet.</h1>
+            <div class="flex justify-between items-center">
+                <button type="button" class=""
+                onclick={handleDelete}>Delete</button>
+                </div>
+    
+                <button
+                type="button"
+                class="flex gap-2 font-medium hover:opacity-70 transition-opacity"
+                onclick={cancelEditing}>Cancel</button>
+    
+                <button
+                type="button"
+                class="flex gap-2 font-medium hover:opacity-70 transition-opacity"
+                onclick={nextStep}>Confirm</button>
+        {/if}
     </div>
 {/if}
 
